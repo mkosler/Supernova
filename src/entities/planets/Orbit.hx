bec6a6966d5133689043f49117a26a6e697510a6
Michael Kosler <marekkpie@gmail.com>
2012-04-21 15:25:12 -0500

    Damn you circle mask

--- src/entities/planets/Orbit.hx
+++ src/entities/planets/Orbit.hx
@@ -5,6 +5,7 @@ import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.HXP;
 import com.haxepunk.masks.Circle;
+import com.haxepunk.utils.Draw;
 
 // Game imports
 import entities.planets.Sun;
@@ -12,28 +13,30 @@ import entities.planets.Planet;
 
 class Orbit extends Entity 
 {
-	private var image : Image;
 	public var numPlanetsHeld : Int;
 	private var maxPlanets : Int;
 	public var radius : Float;
 	private var sun : Sun;
 	private var size : Int;
+	static public inline var baseSize : Int = 128;
 
 	public function new(p_sun : Sun, p_size : Int, p_maxPlanets : Int)
 	{
 		super();
 
 		numPlanetsHeld = 0;
-		size = Std.int(Math.min(Math.max(p_size, 0), 2));
-		image = new Image("gfx/planets/orbit" + size + ".png");
-		graphic = image;
+		size = Std.int(Math.min(Math.max(p_size, 0), 5));
 		maxPlanets = p_maxPlanets;
-		x = p_sun.originX - (image.width / 2);
-		y = p_sun.originY - (image.height / 2);
+		x = p_sun.originX - ((baseSize * p_size) / 2);
+		y = p_sun.originY - ((baseSize * p_size) / 2);
 		setOrigin(p_sun.originX, p_sun.originY);
 		radius = distanceToPoint(Std.int(p_sun.originX), Std.int(p_sun.originY));
 		type = "orbit";
-		mask = new Circle(Std.int(radius), originX, originY);
+		var circle : Circle = new Circle(Std.int(radius), p_sun.originX, p_sun.originY);
+		mask = circle;
+		trace("Sun origin (" + p_sun.originX + ", " + p_sun.originY + ")");
+		trace("Mask position (" + circle.x + ", " + circle.y + ")");
+		sun = p_sun;
 	}
 
 	public function addPlanet(p_planet : Planet) : Bool
@@ -45,5 +48,13 @@ class Orbit extends Entity
 		} else {
 			return false;
 		}
+	}
+
+	public override function render() : Void 
+	{
+		super.render();
+
+		Draw.resetTarget();
+		Draw.circle(sun.originX, sun.originY, Std.int(radius), 0xFFFFFF);
 	}
 }
